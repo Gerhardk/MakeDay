093c05009120c9e3ec2c16ac7b4eada8cc9246b9
Gerhard Koekemoer <gerhard@glucode.com>
2018-10-11 15:31:47 +0200

    Make Day AR THing

--- Assets/ball.cs
+++ Assets/ball.cs
@@ -15,7 +15,7 @@ public class ball : MonoBehaviour {
 	}
 
 	public void ResetPosition () {
-		transform.position = new Vector3(0, 1.2f, 0);
+		transform.localPosition = new Vector3(0, 1.2f, 0);
 		var rigidbody = GetComponent<Rigidbody>();
 		rigidbody.velocity = Vector3.zero;
     rigidbody.angularVelocity = Vector3.zero;
--- Assets/root.cs
+++ Assets/root.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-
 public class root : MonoBehaviour
 {
 
@@ -14,6 +13,12 @@ public class root : MonoBehaviour
   public class Quotes
   {
     public Quote[] values;
+  }
+
+  [System.Serializable]
+  public class PolicyHolder
+  {
+    public string policyholder_id;
   }
 
   [System.Serializable]
@@ -50,9 +55,47 @@ public class root : MonoBehaviour
   public string api_key = "";
   public TextMesh text_mesh;
 
+  // Parameters for creating a policy holder
+  private string firstName = "John";
+  private string lastName = "DOe";
+  private string email = "k@k.com";
+  private string idNumber = "5610035014083";
+
   private void Start()
   {
     // CreateQuote("iPhone 6S 64GB LTE");
+  }
+
+  public void firstNameChanged(string newText) {
+    firstName = newText;
+  }
+
+  public void lastNameChanged(string newText)
+  {
+    lastName = newText;
+  }
+
+  public void emailChanged(string newText)
+  {
+    email = newText;
+  }
+
+  public void idNumberChanged(string newText)
+  {
+    idNumber = newText;
+  }
+
+  public void CreatePolicyHolder() {
+    List<Param> parameters = new List<Param>();
+    parameters.Add(new Param("first_name", firstName));
+    parameters.Add(new Param("last_name", lastName));
+    parameters.Add(new Param("email", email));
+
+    parameters.Add(new Param("id[type]", "id"));
+    parameters.Add(new Param("id[number]", idNumber));
+    parameters.Add(new Param("id[country]", "ZA"));
+
+    StartCoroutine(CallCreatePolicyHolder("https://sandbox.root.co.za/v1/insurance/policyholders", parameters));
   }
 
   public void CreateQuote(string gadget) {
@@ -60,10 +103,10 @@ public class root : MonoBehaviour
     parameters.Add(new Param("type", "root_gadgets"));
     parameters.Add(new Param("model_name", gadget));
 
-    StartCoroutine(CallAPICoroutine("https://sandbox.root.co.za/v1/insurance/quotes", parameters));
+    StartCoroutine(CallGetQuote("https://sandbox.root.co.za/v1/insurance/quotes", parameters));
   }
 
-  IEnumerator CallAPICoroutine(String url, List<Param> parameters)
+  IEnumerator CallGetQuote(String url, List<Param> parameters)
   {
 
     string auth = api_key + ":";
@@ -88,10 +131,64 @@ public class root : MonoBehaviour
     {
       Quotes json = JsonUtility.FromJson<Quotes>("{\"values\":" + www.downloadHandler.text + "}");
 
-      String text = "Make: " + json.values[0].module.make + "\nPremium: R" + (json.values[0].base_premium / 100);
-      Debug.Log("Form upload complete!");
-      Debug.Log(text);
-      text_mesh.text = text;
+      // Getting all packages
+      
+      if (json.values.Length > 0) {
+        int limit = json.values.Length;
+        Debug.Log(limit);
+
+        // This is not working at the moment check out later
+        foreach (var package in json.values) {
+          string text = "";
+          text += "Package Name " + package.package_name;
+          text += "\nMake " + package.module.make + " Model: " + package.module.model;
+          text += "\nPremium: R " + (package.base_premium / 100);
+          text += "\n\n";
+          Debug.Log("+++++");
+          Debug.Log(text);
+          text_mesh.text = text;
+        }
+      } else {
+        text_mesh.text = "There are no Packages for the product : Product X";
+      }  
+    }
+    yield return true;
+  }
+
+  IEnumerator CallCreatePolicyHolder(String url, List<Param> parameters)
+  {
+    string auth = api_key + ":";
+    auth = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(auth));
+    auth = "Basic " + auth;
+
+    var jsonString = "{\"id\": { \"type\": \"id\", \"number\": \"8808125800084\",\"country\": \"ZA\"},\"first_name\": \"Erlich\",\"last_name\": \"Bachman\",\"email\": \"louw+erlich@root.co.za\"}";
+
+    Debug.Log("REQUEST" + jsonString);
+    UnityWebRequest www = UnityWebRequest.Post(url, jsonString);
+    www.SetRequestHeader("AUTHORIZATION", auth);
+    www.SetRequestHeader("Content-Type", "application/json");
+    yield return www.Send();
+
+    Debug.Log(www.isNetworkError);
+    Debug.Log(www.isHttpError);
+
+    if (www.isNetworkError || www.isHttpError)
+    {
+      Debug.Log("ERROR");
+      Debug.Log(www.error);
+      Debug.Log(www.responseCode);
+      Debug.Log(www.downloadHandler.data);
+    }
+    else
+    {
+      Debug.Log("RESPONSE");
+      
+      PolicyHolder json = JsonUtility.FromJson<PolicyHolder>("{\"values\":" + www.downloadHandler.data + "}");
+
+      // Getting policyHolder response
+      
+      Debug.Log("RESPONSE" + json);
+
     }
     yield return true;
   }
